7d636f95783f3ad4405b376ecb1cf6bee2c81bd5
Andre Ramaciotti <andre@ramaciotti.com>
2016-03-26 12:08:29 -0300

    Create gradient images

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ use std::path::Path;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
+const MATCH_COLOR: &'static str = "#ffffff";
 const OFF_COLOR: &'static str = "#222222";
 const ON_COLOR: &'static str = "#ffffff";
 const OUTPUT: &'static str = "output.png";
@@ -25,6 +26,7 @@ const SIZE: u32 = 2048;
 
 /// The options used when creating an image.
 struct ImageOptions {
+    match_color: [u8; 3],
     off_color: [u8; 3],
     on_color: [u8; 3],
     regex: String,
@@ -33,7 +35,12 @@ struct ImageOptions {
 
 /// Create and save the image using the options from the command line.
 fn do_work(matches: Matches) {
-   let off_color = match matches.opt_str("off-color") {
+    let match_color = match matches.opt_str("match-color") {
+        Some(s) => s,
+        None => String::from(MATCH_COLOR)
+    };
+
+    let off_color = match matches.opt_str("off-color") {
         Some(s) => s,
         None => String::from(OFF_COLOR)
     };
@@ -52,9 +59,9 @@ fn do_work(matches: Matches) {
         Some(s) => s.parse::<u32>().unwrap(),
         None => SIZE
     };
-    
 
     let image_options = ImageOptions {
+        match_color: to_color(match_color),
         off_color: to_color(off_color),
         on_color: to_color(on_color),
         regex: matches.free[0].clone(),
@@ -73,11 +80,30 @@ fn create_image(options: ImageOptions) -> RgbImage {
         let pixel_id = pixel_string(x, y, options.size);
 
         if regex.is_match(pixel_id.as_str()) {
-            Rgb(options.on_color)
+            let captures = regex.captures(pixel_id.as_str()).unwrap();
+            let t = match captures.at(1) {
+                Some(c) => c.len() as f64 / pixel_id.len() as f64,
+                None => 0 as f64
+            };
+
+            Rgb(color_lerp(t, options.on_color, options.match_color))
         } else {
             Rgb(options.off_color)
         }
     })
+}
+
+fn color_lerp(t: f64, fst_color: [u8; 3], snd_color: [u8; 3]) -> [u8; 3] {
+    [u8_lerp(t, fst_color[0], snd_color[0]),
+     u8_lerp(t, fst_color[1], snd_color[1]),
+     u8_lerp(t, fst_color[2], snd_color[2])]
+}
+
+fn u8_lerp(t: f64, x: u8, y: u8) -> u8 {
+    let fx = x as f64;
+    let fy = y as f64;
+
+    (fx + t * (fy - fx)) as u8
 }
 
 /// Convert a pixel position into a string as "1121324"
@@ -140,6 +166,7 @@ fn main() {
 
     opts.optopt("", "on-color", "set color for pixels with matches", "COLOR");
     opts.optopt("", "off-color", "set color for pixels without matches", "COLOR");
+    opts.optopt("", "match-color", "set color for pixels colored by match size", "COLOR");
     opts.optopt("o", "output", "set output filename", "FILE");
     opts.optopt("s", "size", "set output image size", "SIZE");
     opts.optflag("h", "help", "print this help menu and quit");
