597dbfc2584f31e5f4fbf2a138f9c675f16170c3
Andre Ramaciotti <andre@ramaciotti.com>
2016-03-26 10:20:09 -0300

    Create empty image

--- README.md
+++ README.md
@@ -4,11 +4,12 @@ Toy project based on [this imgur gallery](https://imgur.com/a/QWMGi).
 
 Completed features:
 
+- Get image size from command line;
+- Read regex from command line;
+- Binary coloring (match/no-match).
+
 Missing features:
 
-- Get image size from command line;
-- Handle rectangular sizes;
-- Read regex from command line;
-- Binary coloring (match/no-match)
-- Gradient coloring (size-of-match/no-match)
+- Handle different shapes;
+- Gradient coloring (size-of-match/no-match).
 
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,95 @@
 extern crate getopts;
+extern crate image;
+extern crate rustc_serialize;
 
-use getopts::Options;
+use getopts::{
+    Matches,
+    Options,
+};
+use image::{
+    Rgb,
+    RgbImage,
+};
+use rustc_serialize::hex::FromHex;
 use std::env;
+use std::path::Path;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+const OFF_COLOR: &'static str = "#222222";
+const ON_COLOR: &'static str = "#ffffff";
+const OUTPUT: &'static str = "output.png";
+const SIZE: u32 = 2048;
+
+/// The options used when creating an image.
+struct ImageOptions {
+    off_color: [u8; 3],
+    on_color: [u8; 3],
+    regex: String,
+    size: u32,
+}
+
+/// Create and save the image using the options from the command line.
+fn do_work(matches: Matches) {
+   let off_color = match matches.opt_str("off-color") {
+        Some(s) => s,
+        None => String::from(OFF_COLOR)
+    };
+
+    let on_color = match matches.opt_str("on-color") {
+        Some(s) => s,
+        None => String::from(ON_COLOR)
+    };
+
+    let output = match matches.opt_str("o") {
+        Some(s) => s,
+        None => String::from(OUTPUT)
+    };
+
+    let size = match matches.opt_str("s") {
+        Some(s) => s.parse::<u32>().unwrap(),
+        None => SIZE
+    };
+    
+
+    let image_options = ImageOptions {
+        off_color: to_color(off_color),
+        on_color: to_color(on_color),
+        regex: matches.free[0].clone(),
+        size: size
+    };
+
+    let image = create_image(image_options);
+    let _ = image.save(&Path::new(&output));
+}
+
+/// Create the image with the given options.
+fn create_image(options: ImageOptions) -> RgbImage {
+    RgbImage::from_fn(options.size, options.size, |x, y| {
+        Rgb(options.off_color)
+    })
+}
+
+/// Print the program usage.
 fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} REGEX [options]", program);
+    let brief = format!("Usage: {} [options] REGEX", program);
     print!("{}", opts.usage(&brief));
 }
 
+/// Print the program version.
+fn print_version(program: &str) {
+    println!("{} v{}", program, VERSION);
+}
+
+/// Convert a string such as "#f0f0f0" or "99cc33" to a u8 array.
+fn to_color(text: String) -> [u8; 3] {
+    let base = text.len() - 6;
+    let hex = text[base..base+6].from_hex().unwrap();
+
+    [hex[0], hex[1], hex[2]]
+}
+
+/// Set up the options and check if usage is correct.
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
@@ -18,7 +100,8 @@ fn main() {
     opts.optopt("", "off-color", "set color for pixels without matches", "COLOR");
     opts.optopt("o", "output", "set output filename", "FILE");
     opts.optopt("s", "size", "set output image size", "SIZE");
-    opts.optflag("h", "help", "print this help menu");
+    opts.optflag("h", "help", "print this help menu and quit");
+    opts.optflag("v", "version", "print this program version and quit");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -30,10 +113,13 @@ fn main() {
         return;
     }
 
-    let output = matches.opt_str("o");
+    if matches.opt_present("v") {
+        print_version(&program);
+        return;
+    }
 
-    let regex = if !matches.free.is_empty() {
-        matches.free[0].clone();
+    if !matches.free.is_empty() {
+        do_work(matches);
     } else {
         print_usage(&program, opts);
         return;
